3fda8d1197646672ce18c40eb1ba616414fd7ac8
Dimitry <legato.di@gmail.com>
2015-09-06 16:44:23 +0300

    Remove trailing semicolon by sprockets to get sourcemaps work

--- back/config/environments/development.rb
+++ back/config/environments/development.rb
@@ -39,3 +39,7 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
+
+Rails.application.assets.unregister_postprocessor "application/javascript", Sprockets::SafetyColons
+
+BetterErrors.editor = :sublime
